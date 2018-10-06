044958c37716b3560a0361d32f5aa32dd0199667
Rasika Phanse <rasikap@Rasikas-MacBook-Air.local>
2018-10-06 21:21:21 +0530

    Change the regex pattern and comment code

--- hacknumber/HackNumber.php
+++ hacknumber/HackNumber.php
@@ -13,11 +13,13 @@ class HackNumber {
   }
 
   public function c_value_yes() {
-    
-    $pattern = "/(?:^|[^a-zA-Z])" . preg_quote($this->string2, '/') . "(?:$|[^a-zA-Z])/i";
+    // Create a pattern which will check if the word is to the start end or a standalone word in a string.
+    $pattern = "/(?:(?<=\s)|(?<=^))" . preg_quote($this->string2, '/') . "(?:(?=\s)|(?=$))/i";
+    // Match the pattern to check if the word exists in the string.
     $exists = preg_match($pattern, $this->string1, $matches, PREG_OFFSET_CAPTURE, $this->initial_pos);
+    // If the word exists, return its first occurence.
     if ($exists) {
-      $string_position = $matches[0][1] + 1;
+      $string_position = $matches[0][1];
     }
     else {
       $string_position = 'No Worries';
@@ -26,8 +28,9 @@ class HackNumber {
   }
 
   public function c_value_no() {
+    // Check if the word exists in the string, need not be standalone word.
     $string_position = stripos($this->string1, $this->string2, $this->initial_pos);
-    if ($string_position) {
+    if (is_int($string_position)) {
       return $string_position;
     }
     else {
--- hacknumber/hacknumbersubmit.php
+++ hacknumber/hacknumbersubmit.php
@@ -8,6 +8,11 @@ $string2 = $_POST['string2'];
 $character = $_POST['character'];
 $initial_position = $_POST['initial_position'];
 
+// Since we are searching word in string, string 2 length should be less than that of string 1.
+if(strlen($string2) > strlen($string1)) {
+  echo 'Length of String 2 cannot be greater than that of String 1';exit;
+}
+
 // Create a class object and pass the parameters to the contructor.
 $hack_no_obj = new HackNumber($string1, $string2, $initial_position);
 
